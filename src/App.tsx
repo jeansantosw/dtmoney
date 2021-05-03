1a35ba38dd5a0c73563a00e6c9d6e44e70c490d5
Jean Santos <jeansantos.sw@gmail.com>
2021-05-03 13:21:11 +0100

    UP

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+//IMPORTACAO DO STYLED COMPONENTS
+
 import { GlobalStyle } from "./styles/global";
+
 
 
 export function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
+     <h1>Hello World DT MONEY</h1>
      <GlobalStyle />
     </div>
   );
