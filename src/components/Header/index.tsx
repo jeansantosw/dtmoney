6ea4a96daf69bfcd97f414cdbbc5ee0656430aa5
Jean Santos <jeansantos.sw@gmail.com>
2021-05-05 12:09:32 +0100

    Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,13 @@
 import logoImg from '../../assets/logo.svg'
+import { Container } from './styles'
 
 export function Header(){
   return(
-    <header>
+    <Container>
      <img src={logoImg} alt="dt money"/>
      <button type="button">
        Nova Transação
      </button>
-    </header>
+    </Container>
   )
 }
