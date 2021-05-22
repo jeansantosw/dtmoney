8f4e14aa7fb436c3455c50e13f92ddf6ad89e1d9
Jean Santos <jeansantos.sw@gmail.com>
2021-05-22 10:19:31 +0100

    up

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -3,6 +3,9 @@ import { Container } from "./styles";
 
 export function TransactionTable(){
   useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+    .then(response => response.json())
+    .then(data => console.log(data))
     
   },[])
 
