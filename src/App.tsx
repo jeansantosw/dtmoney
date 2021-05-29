22af514280fd02d6c6dfffc4b5a861e4d6f02f56
Jean Santos <jeansantos.sw@gmail.com>
2021-05-29 11:29:08 +0100

    Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
 //IMPORTACAO DO STYLED COMPONENTS
-
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
-
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false);
+  }
+  
   return (
     <>
-     <Header />
+     <Header onOpenNewTransectionModal = { handleOpenNewTransactionModal } />
      <Dashboard />
+     
+     <Modal 
+     isOpen={isNewTransactionModalOpen}
+      onRequestClose={handleCloseNewTransactionModal}>
+
+       <h2>Create Transection</h2>
+
+     </Modal>
+
      <GlobalStyle />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransectionModal: () => void;
+}
+
+export function Header({ onOpenNewTransectionModal }: HeaderProps) {
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">
+
+        <button type="button" onClick={ onOpenNewTransectionModal }>
+
           Nova Transação
      </button>
       </Content>
